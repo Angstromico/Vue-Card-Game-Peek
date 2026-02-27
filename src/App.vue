dbda08de06b0d1968b848ee258365545e1b5c619
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-02-27 00:58:54 -0400

    feat: create game board scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CardsGame from './components/CardsGame.vue'
+</script>
 
 <template>
   <h1>Peek a Card</h1>
+  <section class="game-board">
+    <CardsGame />
+  </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.game-container {
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+}
+</style>
---
+++ src/components/CardsGame.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts"></script>
+
+<template lang="">
+  <div class="board">
+    <div v-for="n in 16" :key="n" class="card">
+      {{ n }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.board {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 15px;
+  max-width: 800px;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.card {
+  border: 5px solid #ccc;
+  border-radius: 8px;
+  aspect-ratio: 3 / 4;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  background-color: #f9f9f9;
+  transition: transform 0.2s;
+  cursor: pointer;
+}
+
+.card:hover {
+  transform: scale(1.05);
+  border-color: #42b883;
+}
+</style>
