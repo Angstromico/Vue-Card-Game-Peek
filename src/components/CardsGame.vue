53b4967e7bc86e862b900ce6d6689a6ba4800b7c
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-03-01 17:08:46 -0400

    refactor: use cardInfo as a prop

--- src/components/CardsGame.vue
+++ src/components/CardsGame.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+interface Props {
+  cardInfo: {
+    id: number
+    name: string
+    nameBack: string
+  }[]
+}
+
 interface Card {
   id: number
   name: string
@@ -11,24 +19,8 @@ const choosenCards = ref<Card[]>([])
 const matchedCards = ref<number[]>([])
 const isBusy = ref(false)
 
-const cardInfo = [
-  { id: 1, name: 'Card 1', nameBack: 'Apple' },
-  { id: 2, name: 'Card 2', nameBack: 'Apple' },
-  { id: 3, name: 'Card 3', nameBack: 'Banana' },
-  { id: 4, name: 'Card 4', nameBack: 'Banana' },
-  { id: 5, name: 'Card 5', nameBack: 'Orange' },
-  { id: 6, name: 'Card 6', nameBack: 'Orange' },
-  { id: 7, name: 'Card 7', nameBack: 'Grape' },
-  { id: 8, name: 'Card 8', nameBack: 'Grape' },
-  { id: 9, name: 'Card 9', nameBack: 'Cherry' },
-  { id: 10, name: 'Card 10', nameBack: 'Cherry' },
-  { id: 11, name: 'Card 11', nameBack: 'Mango' },
-  { id: 12, name: 'Card 12', nameBack: 'Mango' },
-  { id: 13, name: 'Card 13', nameBack: 'Berry' },
-  { id: 14, name: 'Card 14', nameBack: 'Berry' },
-  { id: 15, name: 'Card 15', nameBack: 'Lemon' },
-  { id: 16, name: 'Card 16', nameBack: 'Lemon' },
-]
+const props = defineProps<Props>()
+const { cardInfo } = props
 
 const toggleCard = (cardId: number) => {
   if (
